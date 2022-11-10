d9f7eb9acfbe21feb78dbedde13ab4ab5071b50b
Eber Herrera <eber.herrera@xing.com>
2022-11-10 12:07:28 +0100

    basic refactor

---
+++ server/server.js
@@ -0,0 +1,65 @@
+
+const express = require('express')
+const WebSocket = require('ws');
+const { processSlashCommand } = require('./slashCommands');
+
+const WS_PORT = 3030;
+const EXPRESS_PORT = 3333;
+
+const wss = new WebSocket.Server({ port: WS_PORT });
+const app = express();
+
+let hackyConnection;
+
+// WEBSOCKETS SERVER
+wss.on('connection', function connection(ws) {
+  hackyConnection = ws;
+
+  ws.on('message', sendToAll(ws));
+
+  ws.on('message', async (data) => {
+    console.log(data);
+    const dataObj = JSON.parse(data)
+    if (/\/.*/.test(dataObj.message)) {
+      console.log('was a slash command')
+      const response = await processSlashCommand(dataObj.message)
+
+      ws.send(JSON.stringify({
+        name: 'slashCommand',
+        message: JSON.stringify(response)
+      }));
+    }
+  });
+});
+
+const sendToAll = (ws) => (data) => {
+  wss.clients.forEach(function each(client) {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}
+
+//EXPRESS SERVER
+app.use(express.json());
+
+app.post('/oih-receiver', (req, res) => {
+  console.log(req.body)
+  hackyConnection.send(JSON.stringify({name: 'received from OIH', message: JSON.stringify(req.body)}))
+  res.send('OK')
+})
+
+app.get('/callback', (req, res) => {
+  console.log(req.query)
+  hackyConnection.send(JSON.stringify({name: 'callback', message: JSON.stringify(req.query)}))
+  //from here you could trigger a flow that takes a secret id creates a ms teams call
+  res.send(req.query)
+})
+
+app.get('/_health', (req, res) => {
+  res.send('OK')
+})
+
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Example app listening on port ${EXPRESS_PORT}`)
+})
---
+++ server/slashCommands.js
@@ -0,0 +1,75 @@
+const fetch = require('node-fetch');
+
+const USER_TOKEN = 'O2NjzhvEVxSTzgj9YFR8Qu6f5vqFeQp1NoJKcc_3qPDCKYy6lk8d05YC9lB5fo2lAnVivvc70G7FL0zgLfRqe1PJ4c6ib2gAQgGds3V2UwRrqtCJuzFM25BvdJ1Ms072rGQrHodInX480gLk8exyhALsNF_5kMW_3101fQVe9dw';
+
+async function processSlashCommand(request) {
+    const parts = request.split(' ');
+    const command = parts[0];
+    const params = parts.splice(1)
+    console.log('slashCommand Params:', params);
+    
+    switch (command) {
+      case '/auth':
+        return startAuthRequest(params)
+        break;
+      case '/run-flow':
+        return runFlow(params)
+        break;
+      default:
+        console.log('command not recognized ' + command)
+        return 'command not recognized ' + command
+        break;
+    }
+  }
+  
+  async function startAuthRequest(params) {
+  
+    const body = {
+      "scope": "User.Read",
+      "secretName": "anything-secret",
+      "successUrl": "http://localhost:3333/callback"
+    };
+  
+    const response = await fetch('http://webhooks.example.com/hook/636cc8f629df4f8df8c8a70d', {
+      "headers": {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": `Bearer ${USER_TOKEN}`,
+      },
+      "body": JSON.stringify(body),
+      "method": "POST"
+    });
+  
+    const data = await response.json()
+  
+    return data;
+  }
+  
+  async function runFlow(params) {
+  
+    const response = await fetch(`http://webhooks.example.com/hook/${params[0]}`, {
+      "headers": {
+        "accept": "application/json, text/plain, */*",
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "Referer": "http://web-ui.example.com/",
+        "authorization": `Bearer ${USER_TOKEN}`,
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": JSON.stringify(params[1]),
+      "method": "POST"
+    });
+    
+  
+    const data = await response.json()
+  
+    console.log(data)
+  
+    return data;
+  }
+
+  module.exports = {
+    processSlashCommand
+  }
